28077b89839726c50c25d0252e5aec3b5d3d2653
VladaBessikalo <bess.vlada@gmail.com>
2025-07-14 16:02:01 +0200

    feat: add info about author

--- src/features/PostsList.jsx
+++ src/features/PostsList.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postSlice";
+import PostAuthor from "./PostAuthor";
 
 const PostsList = () => {
     const posts = useSelector(selectAllPosts);
@@ -8,6 +9,10 @@ const PostsList = () => {
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content}</p>
+
+            <p>
+              <PostAuthor userId={post.userId}/>
+            </p>
         </article>
     ))
 
