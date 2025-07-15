79f4c0053fee25029594e2587fc2f74db6a018ad
VladaBessikalo <bess.vlada@gmail.com>
2025-07-15 15:23:40 +0200

    feat: add timestamp

--- src/features/PostsList.jsx
+++ src/features/PostsList.jsx
@@ -1,17 +1,20 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postSlice";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
     const posts = useSelector(selectAllPosts);
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = posts.map(post => (
+    const renderedPosts = orderedPosts.map(post => (
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content}</p>
 
             <p>
               <PostAuthor userId={post.userId}/>
+              <TimeAgo timestamp={post.date}/>
             </p>
         </article>
     ))
--- src/features/postSlice.js
+++ src/features/postSlice.js
@@ -1,33 +1,45 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
-    { id: "1", title: "Learning Redux", content: "describe what Redux is and how to use it"},
-    { id: "2", title: "Slices...", content: "describe what Slice is and how to use it in Redux"},
-]
+  {
+    id: "1",
+    title: "Learning Redux",
+    content: "describe what Redux is and how to use it",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+  },
+  {
+    id: "2",
+    title: "Slices...",
+    content: "describe what Slice is and how to use it in Redux",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+  },
+];
 
 const postSlice = createSlice({
-    name: 'posts',
-    initialState,
-    reducers: {
-        postAdded: {
-            reducer(state, action) {
-                state.push(action.payload)
-            },
-            prepare(title, content, userId) {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        title,
-                        content,
-                        userId
-                    }
-                }
-            }
-        }
-    }
-})
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+          },
+        };
+      },
+    },
+  },
+});
 
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postSlice.actions
+export const { postAdded } = postSlice.actions;
 
-export default postSlice.reducer
+export default postSlice.reducer;
