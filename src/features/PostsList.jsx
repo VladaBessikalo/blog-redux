412c36d1e1543cb0f2fc54bb05e1d52e18c0a95c
VladaBessikalo <bess.vlada@gmail.com>
2025-07-16 13:38:09 +0200

    feat: add posts exerpt

--- src/features/PostsList.jsx
+++ src/features/PostsList.jsx
@@ -1,30 +1,36 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "./postSlice";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts} from "./postSlice";
+import PostsExerpt from "./PostsExerpt";
+
 
 const PostsList = () => {
+    const dispatch = useDispatch();
+
     const posts = useSelector(selectAllPosts);
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    const postsStatus = useSelector(getPostsStatus);
+    const error = useSelector(getPostsError);
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
+    useEffect(() => {
+      if (postsStatus === 'idle') {
+        dispatch(fetchPosts())
+      }
+    }, [postsStatus, dispatch])
 
-            <p>
-              <PostAuthor userId={post.userId}/>
-              <TimeAgo timestamp={post.date}/>
-            </p>
-            <ReactionButtons post={post}/>
-        </article>
-    ))
-
+    let content;
+    if(postsStatus === 'loading') {
+      content = <p> "Loading..." </p> // or spinner component
+    } else if (postsStatus === 'succeeded') {
+      const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+      content = orderedPosts.map(post => <PostsExerpt key={post.id} post={post}/>)
+    } else if (postsStatus === 'failed') {
+      content = <p>{error}</p>
+    }
+ 
   return (
     <section>
         <h2>Posts</h2>
-        {renderedPosts}
+        {content}
     </section>
   )
 }
--- src/features/postSlice.js
+++ src/features/postSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios'
+import { sub } from 'date-fns';
 
 const POSTS_URL='https://jsonplaceholder.typicode.com/posts'
 
@@ -11,6 +12,11 @@ const initialState = {
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     const response = await axios.get(POSTS_URL)
+    return response.data
+})
+
+export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost)
     return response.data
 })
 
@@ -49,9 +55,42 @@ const postSlice = createSlice({
       }
     }
   },
+ extraReducers(builder) {
+        builder
+            .addCase(fetchPosts.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                // Adding date and reactions
+                let min = 1;
+                const loadedPosts = action.payload.map(post => {
+                    post.date = sub(new Date(), { minutes: min++ }).toISOString();
+                    post.reactions = {
+                        thumbsUp: 0,
+                        wow: 0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0
+                    }
+                    return post;
+                });
+
+                // Add any fetched posts to the array
+                state.posts = state.posts.concat(loadedPosts)
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
+    }
 });
 
 export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
+
+
 export const { postAdded, reactionAdded } = postSlice.actions;
 
 export default postSlice.reducer;
