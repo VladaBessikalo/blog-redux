5425e3656637cbea1ca2897bb9f2492e9b7c5b92
VladaBessikalo <bess.vlada@gmail.com>
2025-07-16 14:55:21 +0200

    fix: reaction display

--- src/features/postSlice.js
+++ src/features/postSlice.js
@@ -110,7 +110,7 @@ const postSlice = createSlice({
         action.payload.date = new Date().toISOString();
         action.payload.reactions = {
           thumbsUp: 0,
-          hooray: 0,
+          wow: 0,
           heart: 0,
           rocket: 0,
           coffee: 0,
