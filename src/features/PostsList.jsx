8163094e59646e48c4082d3890cf624e566ef36b
VladaBessikalo <bess.vlada@gmail.com>
2025-07-18 14:47:52 +0200

    refactor: fetch posts

--- src/features/PostsList.jsx
+++ src/features/PostsList.jsx
@@ -1,21 +1,12 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts} from "./postSlice";
+import { useSelector } from "react-redux";
+import { selectAllPosts, getPostsStatus, getPostsError} from "./postSlice";
 import PostsExcerpt from "./PostsExcerpt";
 
 
 const PostsList = () => {
-    const dispatch = useDispatch();
-
     const posts = useSelector(selectAllPosts);
     const postStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
-
-    useEffect(() => {
-      if (postStatus === 'idle') {
-        dispatch(fetchPosts())
-      }
-    }, [postStatus, dispatch])
 
     let content;
     if(postStatus === 'loading') {
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import './index.css'
 import App from './App.jsx'
 import {Provider} from 'react-redux'
 import { store } from '../src/store/store'
+import { fetchPosts } from './features/postSlice.js'
 import { fetchUsers } from './features/users/usersSlice.js'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
+store.dispatch(fetchPosts())
 store.dispatch(fetchUsers())
 
 createRoot(document.getElementById('root')).render(
