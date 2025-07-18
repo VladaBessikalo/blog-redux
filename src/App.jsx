ddc634c637a51e4ffa58892707adae3444a0d415
VladaBessikalo <bess.vlada@gmail.com>
2025-07-18 16:05:07 +0200

    feat: create user page

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,28 @@ import PostsList from "./features/PostsList";
 import AddPostForm from "./features/AddPostForm";
 import SinglePostPage from "./features/SinglePostPage";
 import EditPostForm from "./features/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 import Layout from "./components/Layout";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<PostsList/>} />
-          <Route path="post">
-            <Route index element={<AddPostForm/>} />
-            <Route path=":postId" element={<SinglePostPage/>} />
-            <Route path="edit/:postId" element={<EditPostForm/>}/>
-          </Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
-      </Routes>
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header style={{ backgroundColor: "yellow",  padding: "1rem", width: "100%" }}>
+    <header>
         <h1>Redux Blog</h1>
         <nav>
             <ul style={{ listStyle: "none", display: "flex", gap: "1rem" }}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="post">Add Post</Link></li>
+                <li><Link to="user">Users</Link></li>
 
             </ul>
         </nav>
