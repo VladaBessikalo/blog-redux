f903f919a5973042638e7a4b5dab76e6de8be474
VladaBessikalo <bess.vlada@gmail.com>
2025-07-21 16:00:44 +0200

    refactor: avoid unnecessary re-rendering for all posts excerpt

--- src/features/PostsExcerpt.jsx
+++ src/features/PostsExcerpt.jsx
@@ -2,8 +2,9 @@ import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 import { Link } from "react-router-dom";
+import React from "react";
 
-const PostsExcerpt = ({ post }) => {
+let PostsExcerpt = ({ post }) => {
   return (
     <article>
       <h3>{post.title}</h3>
@@ -18,5 +19,7 @@ const PostsExcerpt = ({ post }) => {
     </article>
   );
 };
+
+PostsExcerpt = React.memo(PostsExcerpt)
 
 export default PostsExcerpt;
